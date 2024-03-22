ded234913941ffbc9628153d506d49764878b72c
dkanghan <dharmitanghan@gmail.com>
2024-03-21 21:03:13 -0500

    quick fix

--- app/src/main/java/com/example/wrongsiderocky/MainActivity.java
+++ app/src/main/java/com/example/wrongsiderocky/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.wrongsiderocky;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,7 +20,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
 
         SharedPreferences prefs;
-        SharedPreferences.Editor editor;
         prefs = getSharedPreferences("HiScores", MODE_PRIVATE);
 
 
